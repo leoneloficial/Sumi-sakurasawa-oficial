7133be7d76a6b2464e9012f6f17406c89ce7bf44
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-19 04:47:21 -0400

    Actualizar rpg-trabajar.js

--- plugins/rpg-trabajar.js
+++ plugins/rpg-trabajar.js
@@ -10,7 +10,7 @@ return
 }
 let rsl = Math.floor(Math.random() * 500)
 cooldowns[m.sender] = Date.now()
-await conn.reply(m.chat, `${emoji} ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} 💸.`, m)
+await conn.reply(m.chat, `${emoji} ${pickRandom(trabajo)} *${toNum(rsl)}* ( *${rsl}* ) ${moneda} ⛀.`, m)
 user.coin += rsl
 }
 
