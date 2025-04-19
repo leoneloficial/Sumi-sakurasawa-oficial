3d2b9997c624e2046f896d07b6962b2771b32696
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-19 05:05:37 -0400

    Actualizar rpg-daily.js

--- plugins/rpg-daily.js
+++ plugins/rpg-daily.js
@@ -15,9 +15,9 @@ var handler = async (m, { conn }) => {
     conn.reply(m.chat, `${emoji} *Recompensa Diaria*
 
 Recursos:
-✨ Xp : *+${exp}*
-💎 Diamantes : *+${d}*
-💸 ${moneda} : *+${coin}*`, m);
+❖ Xp : *+${exp}*
+✦ Diamantes : *+${d}*
+⛀ ${moneda} : *+${coin}*`, m);
 
     global.db.data.users[m.sender].lastclaim = Date.now();
 }
@@ -42,3 +42,4 @@ function msToTime(duration) {
 
     return hours + ' Horas ' + minutes + ' Minutos';
 }
+
