aed32c941d69a2c9b655277d610c21c58e6aab8f
Wichi <sebastianbarbaro82@gmail.com>
2025-04-21 23:28:37 -0400

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -1,98 +1,54 @@
-import { watchFile, unwatchFile } from 'fs' 
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
-import fs from 'fs'
-import cheerio from 'cheerio'
-import fetch from 'node-fetch'
-import axios from 'axios'
-import moment from 'moment-timezone' 
+actualizado por Barboza 
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-//BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
-//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumber = '' //Ejemplo: 573218138672
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+import axios from 'axios';
+import moment from 'moment-timezone';
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+global.botNumber = ''; // Ejemplo: 573218138672
 global.owner = [
-  ['393715279301', '🜲 Propietario 🜲', true],
-  ['59169739411'],
-  [''],
-  [''],
-  [''],
-  [''],
-  [''],
-  ['']
+    ['393715279301', '🜲 Propietario 🜲', true],
+    ['59169739411']
 ];
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.mods = []
-global.suittag = ['393715279301'] 
-global.prems = []
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.libreria = 'Baileys'
-global.baileys = 'V 6.7.16' 
-global.vs = '2.2.0'
-global.nameqr = 'YukiBot-MD'
-global.namebot = '✿◟Yυƙι-Sυσυ-Bσƚ◞✿'
-global.sessions = 'Sessions'
-global.jadi = 'JadiBots' 
-global.yukiJadibts = true
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.packname = '⪛✰ sᥙmі - ᑲ᥆𝗍 ✰⪜'
-global.botname = 'ᯓ᮫݃͜ᮨ🌸ܾ݉ᢥsᥙmі sᥲkᥙrᥲsᥲᥕᥲ ꪃꒉܻᵃₚͬ៰'
-global.wm = ''
-global.author = 'Made By ৎ୭࠭͢𓆩𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑𓆪'
-global.dev = '© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ৎ୭࠭͢𓆩𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑𓆪'
-global.textbot = 'sumi sakurasawa • Powered By ৎ୭࠭͢𓆩𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑𓆪'
-global.etiqueta = 'ৎ୭࠭͢𓆩𝕷͢𝖊𝖔፝֟፝֟፝֟፝֟፝֟፝֟𝖓𝖊𝖑𓆪'
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.moneda = 'coin'
-global.welcom1 = '❍ Edita Con El Comando setwelcome'
-global.welcom2 = '❍ Edita Con El Comando setbye'
-global.banner = 'https://files.catbox.moe/a10h2o.jpg'
-global.avatar = 'https://files.catbox.moe/a10h2o.jpg'
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.gp1 = 'https://chat.whatsapp.com/CDw7hpI30WjCyKFAVLHNhZ'
-global.comunidad1 = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-global.channel = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w'
-global.channel2 = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w'
-global.md = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot'
-global.correo = 'thekingdestroy507@gmail.com'
-global.cn ='https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3wa';
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
+global.mods = [];
+global.suittag = ['393715279301'];
+global.prems = [];
+global.libreria = 'Baileys';
+global.baileys = 'V 6.7.16';
+global.vs = '2.2.0';
+global.nameqr = 'YukiBot-MD';
+global.namebot = '✿◟Yυƙι-Sυσυ-Bσƚ◞✿';
+global.sessions = 'Sessions';
+global.jadi = 'JadiBots';
+global.yukiJadibts = true;
+global.packname = '⪛✰ sᥙmі - ᑲ᥆𝗍 ✰⪜';
+global.botname = 'Sumi Sakurasawa';
+global.author = 'Made By ✨ Leo ✨';
+global.dev = 'Powered by Sunflare ☂︎ Team';
+global.textbot = 'Sumi Sakurasawa • Powered by Leo';
+global.banner = 'https://files.catbox.moe/a10h2o.jpg';
+global.avatar = 'https://files.catbox.moe/a10h2o.jpg';
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
-global.ch = {
-ch1: '120363416409380841@newsletter',
-}
-global.multiplier = 70
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-global.cheerio = cheerio
-global.fs = fs
-global.fetch = fetch
-global.axios = axios
-global.moment = moment   
-
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-let file = fileURLToPath(import.meta.url)
+global.gp1 = 'https://chat.whatsapp.com/CDw7hpI30WjCyKFAVLHNhZ';
+global.comunidad1 = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa';
+global.channel = 'https://whatsapp.com/channel/0029Vagdmfv1SWt5nfdR4z3w';
+global.md = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot';
+global.correo = 'thekingdestroy507@gmail.com';
+global.multiplier = 70;
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
+let file = fileURLToPath(import.meta.url);
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'settings.js'"))
-  import(`${file}?update=${Date.now()}`)
-})
+    unwatchFile(file);
+    console.log(chalk.redBright("⚠️ 'config.js' actualizado automáticamente!"));
+    import(`${file}?update=${Date.now()}`);
+});
