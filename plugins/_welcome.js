649ed932108cba16d1c76dd85430e229a53d51bd
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-04-27 22:53:48 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -25,7 +25,7 @@ let redes = ""
 
   if (chat.welcome && m.messageStubType == 27) {
     let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
-    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
+    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, m)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
