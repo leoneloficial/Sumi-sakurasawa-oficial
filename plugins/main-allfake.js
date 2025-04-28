cbedcefd977e4591b5390bb5c986291711ccaf3e
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-28 13:00:33 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -37,7 +37,7 @@ global.namecomu = 'ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥴ᥆mᥙᥒі𝗍ᥡ �
 global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
 
-global.canalIdM = ["120363324350463849@newsletter", "120363324350463849@newsletter"]
+global.canalIdM = ["120363401646371525@newsletter", "120363401646371525@newsletter"]
 global.canalNombreM = ["⏤͟͟͞͞❀ sᥙmі sᥲkᥙrᥲsᥲᥕᥲ • ᥲ᥎іs᥆ ⏤͟͟͞͞❀", "⏤͟͟͞͞❀ sᥙmі sᥲkᥙrᥲsᥲᥕᥲ • ᥲ᥎іs᥆s ⏤͟͟͞͞❀"]
 global.channelRD = await getRandomChannel()
 
