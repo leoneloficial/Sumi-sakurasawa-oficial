969b825699f5348ab2e306790d458251b38d448b
FzTeis <146758681+FzTeis@users.noreply.github.com>
2025-04-27 23:33:00 -0600

    Actualizar grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, isRowner }) => {
   if (!text) return m.reply(`${emoji} Por favor, proporciona una bienvenida para el bot.\n> Ejemplo: #setwelcome Hola user`);
 
-  global.welcom1 = text.trim();
+  global.db.data.settings[conn.user.jid].welcome = text.trim();
   
   m.reply(`${emoji} La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
 };
