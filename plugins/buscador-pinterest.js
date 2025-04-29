fbd4337f496f93da9fd2f1ca2790b9f5ad24a961
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-29 10:53:11 -0400

    Actualizar buscador-pinterest.js

--- plugins/buscador-pinterest.js
+++ plugins/buscador-pinterest.js
@@ -12,7 +12,7 @@ let { pin, created_at, images_url, grid_title } = data
 let HS = `*「✦」 ${grid_title}*
 
 > *✦ creador: » ${created_at}*
-> *🜸 link: » ${pin}`*
+> *🜸 link: » ${pin}*`
 await conn.sendMessage(m.chat, { image: { url: images_url }, caption: HS }, { quoted: m })
 //await conn.sendMessage(m.chat, { image: images_url, caption: HS, footer: '', buttons: [ { buttonId: `.pinterest ${text}`, buttonText: { displayText: 'Siguiente' } }, ], viewOnce: true, headerType: 4 }, { quoted: m })
 
