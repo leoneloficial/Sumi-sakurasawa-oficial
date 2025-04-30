efdbfbe72b414cd471ea2557258c1b6cf66758b0
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-30 12:21:11 -0400

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -13,14 +13,14 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
   if (user.registered === true) return m.reply(`『✦』Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg*`)
-  if (!Reg.test(text)) return m.reply(`『✦』Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
+  if (!Reg.test(text)) return m.reply(`《✧》Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply(`『✦』El nombre no puede estar vacío.`)
-  if (!age) return m.reply(`『✦』La edad no puede estar vacía.`)
-  if (name.length >= 100) return m.reply(`『✦』El nombre es demasiado largo.`)
+  if (!name) return m.reply(`《✧》El nombre no puede estar vacío.`)
+  if (!age) return m.reply(`《✧》La edad no puede estar vacía.`)
+  if (name.length >= 100) return m.reply(`《✧》El nombre es demasiado largo.`)
   age = parseInt(age)
-  if (age > 1000) return m.reply(`『✦』Wow el abuelo quiere jugar al bot.`)
-  if (age < 5) return m.reply(`『✦』hay un abuelo bebé jsjsj.`)
+  if (age > 1000) return m.reply(`《✧》Wow el abuelo quiere jugar al bot.`)
+  if (age < 5) return m.reply(`《✧》hay un abuelo bebé jsjsj.`)
   user.name = name + '✓'.trim()
   user.age = age
   user.regTime = + new Date      
