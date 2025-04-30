26d494fa9f9fe3c1fe615b72109947e462e0bd77
lucashokage <lucashokage25@gmail.com>
2025-04-30 14:01:00 -0500

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,15 +1,23 @@
-let handler = async (m, { conn, text, isRowner }) => {
-  if (!text) return m.reply(`${emoji} Por favor, proporciona una bienvenida para el bot.\n> Ejemplo: #setwelcome Hola user`);
-
-  global.db.data.settings[conn.user.jid].welcome = text.trim();
+let handler = async (m, { conn, text, isAdmin, isOwner }) => {
   
-  m.reply(`${emoji} La bienvenida del bot ha sido cambiado a: ${global.welcom1}`);
+  const defaultWelcome = 'Bienvenid@ al grupo @user';
+  
+  if (!text) {
+    global.db.data.settings[conn.user.jid].welcome = defaultWelcome;
+    return m.reply(`${emoji} Se ha establecido la bienvenida predeterminada:\n\n${defaultWelcome.replace('@user', '@' + m.sender.split('@')[0])}`);
+  }
+  
+  const fullWelcome = `${defaultWelcome}\n\n${text.trim()}`;
+  global.db.data.settings[conn.user.jid].welcome = fullWelcome;
+  
+  m.reply(`${emoji} La bienvenida se ha actualizado correctamente:\n\n${fullWelcome.replace('@user', '@' + m.sender.split('@')[0])}`);
 };
 
-handler.help = ['setwelcome'];
-handler.tags = ['tools'];
+handler.help = ['setwelcome <texto>'];
+handler.tags = ['group'];
 handler.command = ['setwelcome'];
 handler.owner = false;
 handler.admin = true;
+handler.group = true;
 
 export default handler;
