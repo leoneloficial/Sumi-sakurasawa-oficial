1e856a8f6bef6bf58e954a2de89561beafd1d651
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-30 12:43:51 -0400

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -448,7 +448,7 @@ conn.reply(m.chat, `《✧》 Se agotaron tus ${moneda}`, m)
 continue
 }
 if (plugin.level > _user.level) {
-conn.reply(m.chat, `❮✦❯ Se requiere el nivel: *${plugin.level}*\n\n• Tu nivel actual es: *${_user.level}*\n\n• Usa este comando para subir de nivel:\n*${usedPrefix}levelup*`, m)       
+conn.reply(m.chat, `《✧》 Se requiere el nivel: *${plugin.level}*\n\n• Tu nivel actual es: *${_user.level}*\n\n• Usa este comando para subir de nivel:\n*${usedPrefix}levelup*`, m)       
 continue
 }
 let extra = {
