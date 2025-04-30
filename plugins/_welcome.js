a79c24c258989b78a73485c50e61be143c69fe7e
lucashokage <lucashokage25@gmail.com>
2025-04-30 14:48:42 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -2,36 +2,65 @@ import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
-let bot = global.db.data.settings[conn.user.jid]
-
-global.botname = global.db.data.settings[conn.user.jid].botName || global.bottname
-
-global.textbot = `${botname} Powered by ${conn.getName(conn.user.jid)}` || global.textobot
-
-global.welcom1 = bot.welcome || welcomm1
-global.welcom2 = bot.bye || welcomm2
-
-let redes = ""
-
+  let bot = global.db.data.settings[conn.user.jid]
+  
   if (!m.messageStubType || !m.isGroup) return !0;
+  
+  
   let pp = bot.logo.welcome || await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/un7lt7.jpg')
-  let img = await (await fetch(`${pp}`)).buffer()
+  let img = await (await fetch(pp)).buffer()
   let chat = global.db.data.chats[m.chat]
-  let txt = '❀⏤͟͟͞͞BIENVENIDO‹࣭݊𓂃ⷪ ִֶָ ᷫ‹ ⷭ.࣭𓆩‌۫᷼Ⴕ۫͜𓆪‌'
-  let txt1 = '❀⏤͟͟͞͞ADIOS‹࣭݊𓂃ⷪ ִֶָ ᷫ‹ ⷭ.࣭𓆩‌۫᷼Ⴕ۫͜𓆪‌'
+  let groupName = groupMetadata.subject
   let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
-    groupSize--;
+  
+  
+  if (m.messageStubType == 27) { 
+    groupSize++
+  } else if (m.messageStubType == 28 || m.messageStubType == 32) { // Salida
+    groupSize--
   }
+
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = bot.welcome || `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
-    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, null)
+    let user = m.messageStubParameters[0]
+    let username = '@' + user.split('@')[0]
+    
+    
+    let welcomeMsg = chat.sWelcome || `❀ *Bienvenido* a %group\n✰ @user\n%text\n✦ Ahora somos %members Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!`
+    
+    // Reemplazar variables
+    let finalMsg = welcomeMsg
+      .replace(/@user/g, username)
+      .replace('%group', groupName)
+      .replace('%text', global.welcom1 || '')
+      .replace('%members', groupSize)
+    
+    
+    await conn.sendMessage(m.chat, {
+      text: finalMsg,
+      mentions: [user]
+    }, { quoted: m })
+    
+    
+    await conn.sendFile(m.chat, img, 'welcome.jpg', '', m, false, { mentions: [user] })
   }
+
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = bot.bye || `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
-    await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, null)
-  }}
+    let user = m.messageStubParameters[0]
+    let username = '@' + user.split('@')[0]
+    
+    let byeMsg = chat.sBye || `❀ *Adiós* de %group\n✰ @user\n%text\n✦ Ahora somos %members Miembros.\n•(=^●ω●^=)• Te esperamos pronto!`
+    
+    let finalMsg = byeMsg
+      .replace(/@user/g, username)
+      .replace('%group', groupName)
+      .replace('%text', global.welcom2 || '')
+      .replace('%members', groupSize)
+    
+    await conn.sendMessage(m.chat, {
+      text: finalMsg,
+      mentions: [user]
+    }, { quoted: m })
+  }
+}
